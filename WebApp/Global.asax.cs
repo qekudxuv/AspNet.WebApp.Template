f16f8a1d997c6350f54fd88e1b1c3de8a5139d1b
sam.sh_chang <sam.sh_chang@htc.com>
2017-08-23 18:02:08 +0800

    prevent Cross-Frame Scripting(Clickjacking)

--- WebApp/Global.asax.cs
+++ WebApp/Global.asax.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
 
 namespace WebApp
 {
@@ -11,6 +7,12 @@ namespace WebApp
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            //to prevent Cross-Frame Scripting(Clickjacking)
+            HttpContext.Current.Response.AddHeader("x-frame-options", "SAMEORIGIN");
         }
     }
 }
